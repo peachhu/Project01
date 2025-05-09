c6a0b5e562a1ad8867ca6bfbf10f89ad921cd019
Chanunchita Suphachan <peachlyhoneyy@gmail.com>
2025-05-09 23:07:52 +0700

    add authorize function

--- middleware/auth.js
+++ middleware/auth.js
@@ -32,3 +32,14 @@ exports.protect=async (req,res,next) =>{
     }
 
 }
+
+//Grant access to a specific role
+exports.authorize= (...roles)=>{
+return(req,res,next)=>{
+if(!roles.includes(req.user.role)){
+  return res.status(403).json({success: false, msg: `User role ${req.user.role} is not authorized to access this route.`});
+}
+next();
+}
+
+}
--- routes/menuitems.js
+++ routes/menuitems.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const {getMenuitem,getMenuitems,updateMenuitem,createMenuitem,deleteMenuitem} = require('../controllers/menuitems')
 const router = express.Router();
-const {protect} = require('../middleware/auth');
+const {protect,authorize} = require('../middleware/auth');
 
 
-router.route('/').get(getMenuitems).post(protect,createMenuitem);
-router.route('/:id').get(getMenuitem).put(protect,updateMenuitem).delete(protect,deleteMenuitem)
+router.route('/').get(getMenuitems).post(protect,authorize('admin'),createMenuitem);
+router.route('/:id').get(getMenuitem).put(protect,authorize('admin'),updateMenuitem).delete(protect,authorize('admin'),deleteMenuitem)
 
 module.exports = router;
