09d20ac55688110d0f58b6996d5338308ad3c77d
Chanunchita Suphachan <peachlyhoneyy@gmail.com>
2025-05-12 15:35:42 +0700

    create a  select and sort rest api

--- controllers/menuitems.js
+++ controllers/menuitems.js
@@ -1,16 +1,52 @@
 const Menuitem = require("../models/Menuitem");
+const asyncHandler = require('express-async-handler');
+
 
 //Get all Menuitems
 // route => GET /api/v1/menuitems
 //access public
-exports.getMenuitems = async (req, res, next) => {
-  try {
-    const menues = await Menuitem.find();
-    res.status(200).json({ success: true, count: menues.length, data: menues });
-  } catch (err) {
-    res.status(400).json({success:false});
+exports.getMenuitems = asyncHandler(async(req, res, next) => {
+  
+    let query;
+
+    //Copy req.query
+    const reqQuery = {...req.query};
+
+    //Fields to exclude
+    const removeFields=['select','sort']
+
+    //Loop over remove fields and delete them from reqQuery
+    removeFields.forEach(param=>delete reqQuery[param]);
+    console.log(reqQuery);
+
+    //Creqte query string
+    let queryStr = JSON.stringify(reqQuery);
+
+    queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g , match=>`$${match}`);
+
+    let queryObj = JSON.parse(queryStr);
+
+    query = Menuitem.find(queryObj)
+    
+  //Select Fields
+  if(req.query.select){
+    const selected_fields = req.query.select.split(',').join(' ');
+    query=query.select(selected_fields);
   }
-};
+
+  //Sort
+   if(req.query.sort){
+    const sortBy = req.query.sort.split(',').join(' ');
+    query=query.sort(sortBy);
+  }else{
+     query=query.sort('-createdAt');
+  }
+
+    const menues = await Menuitem.find(query);
+
+    res.status(200).json({ success: true, count: menues.length, data: menues });
+
+});
 
 //Get single menuitem
 // route => GET /api/v1/menuitems/:id
